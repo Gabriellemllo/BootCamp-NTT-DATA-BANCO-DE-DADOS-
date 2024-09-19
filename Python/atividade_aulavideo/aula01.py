a6029488f6fda14cc9aafd0a2d391c8e60021a87
Gabi Melo <gabriellesilvamelo622@gmail.com>
2024-09-19 18:25:47 -0300

    1

---
+++ Python/atividade_aulavideo/aula01.py
@@ -0,0 +1,19 @@
+
+#Aula01 tipos de dados
+
+# tipo int(inteiro)
+print(11+10+100)
+
+# tipo float (decimal)
+print(1.5 + 1.5)
+
+# tipo bool (Booleano)
+print(True)
+print(False)
+
+# tipo str/palavra (string)
+print("python")
+
+
+
+# curiosidade : crie um novo terminal e digite python + enter : você vai criar uma calculadora.
